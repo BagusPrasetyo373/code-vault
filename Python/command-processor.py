2cfb0a50f80ead604fe2aa623baccb92598a2084
HugeBrain16 <bagusidm373@gmail.com>
2021-02-07 02:41:05 -0800

    Update command-processor.py

--- Python/command-processor.py
+++ Python/command-processor.py
@@ -1,10 +1,12 @@
 import os # a module for os thingy
+import re # regex
 
 # every printed error message is pretty self explanatory, just read whats the cause of getting the error message
 # just a bit lazy of explaining if its actually self explanatory
 
 args = [] # command arguments
 get_args = None # get arguments
+max_cmd_args = 1024
 cmdlist = ["exit","quit","help"] # list of commands
 
 while True: # while loop ;\
@@ -12,7 +14,9 @@ while True: # while loop ;\
 	if not get_args: print("command is empty!") # if command is empty
 	else:
 		get_args.lower() # to lowercase
-		args = get_args.split(" ") # split the arguments then put them on the list
+		args = re.split(" +",get_args) # split the arguments then put them on the list
+		for i in range(len(get_args),max_cmd_args):
+			args.insert(i,None)
 		if(args[0] == "exit" or args[0] == "quit"): # command example for quit or exit
 			break
 		elif(args[0] == "help"): # -
